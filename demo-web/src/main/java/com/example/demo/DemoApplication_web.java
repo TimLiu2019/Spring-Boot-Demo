6b357001cc38a34e9dffb50ff7aa32ac2f94e7e5
TimLiu2019 <qitingliu7@gmail.com>
2021-02-21 18:49:43 -0500

    initial spring boot web

---
+++ demo-web/src/main/java/com/example/demo/DemoApplication_web.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication_web {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication_web.class, args);
+    }
+
+}
