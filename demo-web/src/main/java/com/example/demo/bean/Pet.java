a6f988466e4bec98c54024617868ee9b060b3f95
TimLiu2019 <qitingliu7@gmail.com>
2021-03-02 21:21:28 -0500

    costomer convert

--- demo-web/src/main/java/com/example/demo/bean/Pet.java
+++ demo-web/src/main/java/com/example/demo/bean/Pet.java
@@ -1,11 +1,13 @@
 package com.example.demo.bean;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Data
 public class Pet {
-
     private String name;
     private Integer age;
-
 }
