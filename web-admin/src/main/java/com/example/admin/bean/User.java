a8802adfd9620dfff3cbbc8699151bc860a47236
TimLiu2019 <qitingliu7@gmail.com>
2021-03-06 21:39:40 -0500

    thymeleaf traverse data

--- web-admin/src/main/java/com/example/admin/bean/User.java
+++ web-admin/src/main/java/com/example/admin/bean/User.java
@@ -1,7 +1,11 @@
 package com.example.admin.bean;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class User {
     private String userName;
--- web-admin/src/main/java/com/example/admin/controller/TableController.java
+++ web-admin/src/main/java/com/example/admin/controller/TableController.java
@@ -1,8 +1,13 @@
 package com.example.admin.controller;
 
 
+import com.example.admin.bean.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class TableController {
@@ -13,7 +18,11 @@ public class TableController {
     }
 
     @GetMapping("/dynamic_table")
-    public String dyanmic_table(){
+    public String dyanmic_table(Model model){
+
+      List<User> users =  Arrays.asList(new User("Tom","123456"),new User("Jerry","123456"),
+        new User("John","123456"),new User("Mike","123456"));
+       model.addAttribute("users",users);
         return "table/dynamic_table";
     }
 
